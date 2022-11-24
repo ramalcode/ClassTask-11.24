4860f6b3db98906eb9f935f5ad0657cce60c022c
ramalfatullayevh <tu7ma9clt@code.edu.az>
2022-11-24 08:45:28 +0400

    added c# files

---
+++ Task/NewFolder/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.NewFolder
+{
+    internal class Product
+    {
+		private string _productname;
+
+		public string Name
+		{
+			get => _productname;
+			set 
+			{
+				if (value.Trim().Length>1)
+				{
+					_productname = value.Trim(); 
+
+				}
+				
+			}
+		}
+
+		private float _productprice;
+
+		public float Price
+		{
+			get => _productprice; 
+			set 
+			{
+				if (value>0)
+				{
+					_productprice = value;
+
+				}
+				
+			}
+		}
+
+
+	}
+}
---
+++ Task/NewFolder/User.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.NewFolder
+{
+    internal class User
+    {
+		private Product[] _product;
+		private string _username;
+
+		public string Username
+		{
+			get => _username; 
+			set 
+			{
+				if (value.Trim().Length>=6 && value.Trim().Length <= 25)
+
+				{
+					_username = value.Trim();
+
+				}
+				
+			}
+		}
+
+		private string _password;
+
+		public string Password
+		{
+			get => _password; 
+			set 
+			{
+				if (value.Trim().Length >= 8 && value.Trim().Length <= 25 && CheckDigit(value) && CheckLower(value) && CheckUpper(value))
+				{
+					_password = value.Trim(); 
+
+				}
+				
+			}
+		}
+
+		private bool _isSignedIn;
+
+		public bool IsSignedIn
+		{
+			get =>_isSignedIn; 
+			set 
+			{ 
+				_isSignedIn = value;
+			}
+		}
+
+
+		public bool CheckDigit(string password)
+		{
+			for (int i = 0; i < password.Length; i++)
+			{
+				var result= Char.IsDigit(password[i]);
+
+				if (result)
+				{
+					return true;
+				}
+				else
+				{
+					Console.WriteLine("wrong input");
+				}
+			} return false;
+
+
+		}
+		public bool CheckLower( string password)
+		{
+			for (int i = 0; i < password.Length; i++)
+			{
+				var result = Char.IsLower(password[i]);
+
+				if (result)
+				{
+					return true;
+				}
+				else
+				{
+					Console.WriteLine("Wrong input");
+				}
+			} return false;
+		}
+
+		public bool CheckUpper(string password)
+		{
+			for (int i = 0; i < password.Length; i++)
+			{
+				var result =Char.IsUpper(password[i]);
+
+				if (result)
+				{
+					return true;
+
+				}
+				else
+				{
+					
+				}
+			} return false;
+		}
+
+		public void Login()
+		{
+			Username = Console.ReadLine();
+			Password = Console.ReadLine();
+		}
+
+		public void Add()
+		{
+			if (Username!=null  && Password!=null)
+			{
+
+			}
+
+			else
+			{
+				Login();
+			}
+		}
+
+
+	}
+}
